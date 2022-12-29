b6cc87976e20a4e711c87058d9f7d805484843e4
abdulrhman elfallah <abdohegag@gmail.com>
2022-12-29 03:11:24 +0200

    create config

---
+++ config/config.py
@@ -0,0 +1,12 @@
+##NOTE -  this is a config file we use to store configuration while app run
+
+## database file path var
+databaseFilePath=''
+## database file name 
+databaseFileName=''
+
+## database sets
+dataList=list()
+
+## eclat report
+eclatReport:list[int]=list()
